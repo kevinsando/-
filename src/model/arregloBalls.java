fe6fe6d62c91ee59007b608aadfc72a23387c163
Kevin <kevin_sm20@hotmail.com>
2019-08-28 10:08:45 -0600

    cambio completo

---
+++ src/model/arregloBalls.java
@@ -0,0 +1,18 @@
+
+package model;
+import controller.Ball;
+
+public class arregloBalls {
+    private Ball[] arreglo;
+    private int n=10;
+    private int k=0;
+
+    public arregloBalls() {
+        this.k=0;
+        arreglo = new Ball[n];
+    }
+    
+    
+    
+    
+}
